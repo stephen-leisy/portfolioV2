4ece9e6605b5786cfb8198da14cf36919c2ea6af
Stephen Leisy <stleisy@gmail.com>
2024-11-19 09:55:59 -0800

    Cleaned up inital next.js code and added Chakra UI.

---
+++ src/features/landing/LandingPage.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Center } from '@chakra-ui/react';
+
+export const LandingPage = () => {
+  return (
+    <Center h="100vh" w="full">
+      Hi world! !!!
+    </Center>
+  );
+};
