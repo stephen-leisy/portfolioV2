b41b12b2acf6551018d6768163410298f4eeab3a
Stephen Leisy <stleisy@gmail.com>
2024-12-10 12:41:20 -0800

    Quick page changes 3.

--- src/app/tunes/page.tsx
+++ src/app/tunes/page.tsx
@@ -7,7 +7,7 @@ const page: React.FC = () => (
     columnOne={loremIpsum[0]}
     columnTwo={loremIpsum[1]}
     imgUrl="https://i.ibb.co/LznG7VG/9-BA46-C93-486-C-443-B-9-ED3-051-D0914-B948.jpg"
-    imageWidth="55%"
+    imageWidth="45%"
   />
 );
 
