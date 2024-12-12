018aeca1e893431d80659c505f68b041ed651bc8
Stephen Leisy <stleisy@gmail.com>
2024-12-12 12:27:46 -0800

    tunes page built4.

--- src/app/tunes/page.tsx
+++ src/app/tunes/page.tsx
@@ -7,11 +7,11 @@ const page: React.FC = () => (
   <PageWrapper header="Tunes">
     <Text fontSize="xs" fontWeight={500}>
       Outside of development & design, music holds one of the largest spots in
-      my heart. I've been playing in bands, touring in ancient vans (not so much
-      these days, tho), and recording/producing/mixing songs since I was a
+      my heart. I&apos;ve been playing in bands, touring in ancient vans (not so
+      much these days, tho), and recording/producing/mixing songs since I was a
       teenager and am still gigging around town in Portland, OR with my current
-      project 'Sherbet Tone'. Here's a sampling of some of my favorite tunes
-      from bands I've been in.
+      project &apos;Sherbet Tone&apos;. Here&apos;s a sampling of some of my
+      favorite tunes from bands I&apos;ve been in.
     </Text>
     {tunesConfig.map((props) => (
       <ProjectCard {...props} key={props.iframeSource} />
