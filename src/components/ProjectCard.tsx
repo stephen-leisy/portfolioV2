e14ce00d894b7f18386dd9f8c28bdcfc669be9d2
Stephen Leisy <stleisy@gmail.com>
2024-12-13 13:58:25 -0800

    Loading skeletons.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { IProjectConfig } from '@/configs/config-types/projectConfigTypes';
-import { Flex, Text, Image, Link } from '@chakra-ui/react';
+import { Flex, Text, Image, Link, Skeleton } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export const ProjectCard: React.FC<IProjectConfig> = ({
   title,
@@ -9,55 +10,81 @@ export const ProjectCard: React.FC<IProjectConfig> = ({
   linkDisplayTitle,
   img2,
   iframeSource,
-}) => (
-  <Flex
-    flexDirection="column"
-    gap={{ base: 4, lg: 8 }}
-    pb={16}
-    bg="gray.100"
-    p={{ base: 4, lg: 8 }}
-    borderRadius="lg"
-    _dark={{ bg: 'gray.700' }}
-  >
+}) => {
+  const [imageIsLoaded, setImageIsLoaded] = useState<boolean>(false);
+  const [secondImageIsLoaded, setSecondImageIsLoaded] =
+    useState<boolean>(false);
+  return (
     <Flex
-      w="full"
-      gap={{ base: 2, lg: 4 }}
-      flexDir={{ base: 'column', lg: 'row' }}
-      fontSize={{ base: 'sm', lg: 'md' }}
+      flexDirection="column"
+      gap={{ base: 4, lg: 8 }}
+      pb={16}
+      bg="gray.100"
+      p={{ base: 4, lg: 8 }}
+      borderRadius="lg"
+      _dark={{ bg: 'gray.700' }}
     >
-      <Text
-        fontWeight={600}
-        color="pink.300"
-        textAlign={{ base: 'right', lg: 'left' }}
+      <Flex
+        w="full"
+        gap={{ base: 2, lg: 4 }}
+        flexDir={{ base: 'column', lg: 'row' }}
+        fontSize={{ base: 'sm', lg: 'md' }}
       >
-        {title}
-      </Text>
-      <Text display={{ base: 'none', lg: 'block' }}>|</Text>
-      <Text textAlign={{ base: 'right', lg: 'left' }}>
-        <Link
-          color="gray.500"
-          _dark={{ color: 'gray.100' }}
-          href={link}
-          target="blank"
-          w="fit-content"
+        <Text
+          fontWeight={600}
+          color="pink.300"
+          textAlign={{ base: 'right', lg: 'left' }}
         >
-          {linkDisplayTitle}
-        </Link>
-      </Text>
+          {title}
+        </Text>
+        <Text display={{ base: 'none', lg: 'block' }}>|</Text>
+        <Text textAlign={{ base: 'right', lg: 'left' }}>
+          <Link
+            color="gray.500"
+            _dark={{ color: 'gray.100' }}
+            href={link}
+            target="blank"
+            w="fit-content"
+          >
+            {linkDisplayTitle}
+          </Link>
+        </Text>
+      </Flex>
+      {img1 && (
+        <Skeleton loading={!imageIsLoaded}>
+          <Image
+            borderRadius="lg"
+            w="full"
+            src={img1}
+            alt={img1}
+            loading="lazy"
+            onLoad={() => setImageIsLoaded(true)}
+          />
+        </Skeleton>
+      )}
+      {img2 && (
+        <Skeleton loading={!secondImageIsLoaded}>
+          <Image
+            borderRadius="lg"
+            w="full"
+            src={img2}
+            alt={img2}
+            loading="lazy"
+            onLoad={() => setSecondImageIsLoaded(true)}
+          />
+        </Skeleton>
+      )}
+      {iframeSource && (
+        <Skeleton loading={!imageIsLoaded}>
+          <iframe
+            width="100%"
+            height="166"
+            allow="autoplay"
+            src={iframeSource}
+            onLoad={() => setImageIsLoaded(true)}
+          ></iframe>
+        </Skeleton>
+      )}
     </Flex>
-    {img1 && (
-      <Image borderRadius="lg" w="full" src={img1} alt={img1} loading="lazy" />
-    )}
-    {img2 && (
-      <Image borderRadius="lg" w="full" src={img2} alt={img2} loading="lazy" />
-    )}
-    {iframeSource && (
-      <iframe
-        width="100%"
-        height="166"
-        allow="autoplay"
-        src={iframeSource}
-      ></iframe>
-    )}
-  </Flex>
-);
+  );
+};
