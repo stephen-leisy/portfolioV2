fbcd18570caa62ea2c1334ced7669910b84a412e
Stephen Leisy <stleisy@gmail.com>
2025-01-09 13:18:17 -0800

    Simple load animation 2.

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,35 +1,5 @@
-'use client';
-import { AnimationWrapper } from '@/components/ui/AnimationWrapper';
-import { linksConfig } from '@/configs/linksConfig';
-import { useLoadAnimationTimer } from '@/hooks/useLoadAnimationTimer';
-import { Flex, Link } from '@chakra-ui/react';
+import { LinksPage } from '@/components/LinksPage';
 
-const page: React.FC = () => {
-  const loadAnimationInProgress = useLoadAnimationTimer();
-
-  return (
-    <AnimationWrapper animationSwitch={!loadAnimationInProgress}>
-      <Flex flexDirection="column" alignItems="flex-end" gap={2}>
-        {linksConfig.map(({ link, displayTitle }) => (
-          <Link
-            color="gray.800"
-            _dark={{ color: 'gray.100' }}
-            href={link}
-            target="blank"
-            w="fit-content"
-            textDecoration="none"
-            fontWeight={500}
-            key={displayTitle}
-            _hover={{
-              color: 'pink.300',
-            }}
-          >
-            {displayTitle}
-          </Link>
-        ))}
-      </Flex>
-    </AnimationWrapper>
-  );
-};
+const page: React.FC = () => <LinksPage />;
 
 export default page;
